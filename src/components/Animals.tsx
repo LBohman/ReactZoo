bef5734934769e4245d99ed38320387316f4e613
Lovisa Bohman <lovisabo@hotmail.com>
2021-06-12 13:03:56 +0200

    Add Animals-component to App.tsx and display amount of animals on screen

--- src/components/Animals.tsx
+++ src/components/Animals.tsx
@@ -1,3 +1,19 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Animal } from "../models/Animal";
+
 export const Animals = () => {
-    return (<div>Displaying component Animals.tsx on screen.</div>);
+    let defaultValue: Animal[] = [];
+    const [animals, setAnimals] = useState(defaultValue);
+
+    useEffect(() => {
+        //ngOnInit i angular
+
+        axios.get<Animal[]>('https://animals.azurewebsites.net/api/animals').then(response => {
+            setAnimals(response.data);
+            console.log(response.data);
+        })
+    }, []);
+    //return (<div>Displaying component Animals.tsx on screen.</div>);
+    return (<div>Found: {animals.length}</div>);
 }
