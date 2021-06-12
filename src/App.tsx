16f926492aaa4abce4e2bfcdca73e498b13791c3
Lovisa Bohman <lovisabo@hotmail.com>
2021-06-12 16:25:17 +0200

    Add routing for components Animals, Animal and PageNotFound

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Animals } from './components/Animals';
+import { Animal } from './components/Animal';
+import { PageNotFound } from './components/PageNotFound';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <Animals></Animals>
-    </div>
+    <Router>
+      <Switch>
+        {/* Startsidan, alla djur listas */}
+        <Route exact path="/">
+          <Animals></Animals>
+        </Route>
+        {/* Ett individuellt djur presenteras, mer detaljer */}
+        <Route path="/animal/:id">
+          <Animal></Animal>
+        </Route>
+        {/* Om något går fel/sida kan inte hittas */}
+        <Route path="*">
+          <PageNotFound></PageNotFound>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
