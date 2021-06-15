f0ef7dc056ba6f4d342865bcdccbc6da76d87d23
Lovisa Bohman <lovisabo@hotmail.com>
2021-06-15 20:56:02 +0200

    Add localStorage functionality, now with less API-calls near you!

--- src/components/Animals.tsx
+++ src/components/Animals.tsx
@@ -7,20 +7,49 @@ export const Animals = () => {
     let defaultValue: Animal[] = [];
     const [animals, setAnimals] = useState(defaultValue);
 
+    //ngOnInit i angular
     useEffect(() => {
-        //ngOnInit i angular
 
-        axios.get<Animal[]>('https://animals.azurewebsites.net/api/animals').then(response => {
+        if(localStorage.animalList) {
+            fetchFromLS();
+            console.log("if-statement activate");
+        } else {
+            axios.get<Animal[]>('https://animals.azurewebsites.net/api/animals')
+            .then(response => {
             setAnimals(response.data);
-            console.log(response.data);
+            saveToLS((response.data));
         })
+            console.log("else-statement activate");
+        }
+
     }, []);
+
+    function fetchFromLS() {
+        let listFromLS = localStorage.getItem('animalList');
+        if(listFromLS === null) {
+            console.log("LS is empty");
+        } else {
+            let animals = JSON.parse(listFromLS);
+            setAnimals(animals);
+        }
+    }
+
+    function saveToLS(responseApi: Animal[]) {
+        for (let i = 0; i < responseApi.length; i++) {
+            const element = responseApi[i];
+            defaultValue.push(element);
+            }
+        let stringifiedList = JSON.stringify(defaultValue);
+        localStorage.setItem("animalList", stringifiedList);
+    }
 
     let liTags = animals.map((animal) => {
         return (
             <li key={animal.id}>
-                <h4>{animal.name}</h4>
-                <p>{animal.shortDescription}</p>
+                <h3>{animal.name}</h3>
+                <div>
+                    <p>{animal.shortDescription}</p>
+                </div>
                 <Link to={"/animal/" + animal.id}>Läs mer om {animal.name}</Link>
             </li>
         )
